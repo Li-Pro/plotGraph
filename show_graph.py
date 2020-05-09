51df6ea3b9d268c0b18d0c7d6079d717c5b06d8d
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-05-09 15:58:56 +0800

    Fixing updating funcs.

--- show_graph.py
+++ show_graph.py
@@ -2,40 +2,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import threading, time
+import code
 
-# plot_data = {'devx'=[], 'devy'=[], 'lock'=threading.Lock()}
-plot_data = type('PlotData', (object,), dict(devx=[], devy=[], lock=threading.Lock())) ( )
+plot_data = type('PlotData', (object,), dict(devx=[], devy=[], needRedraw=False, lock=threading.Lock())) ( )
+shell_exit = False
 
 def setFunction(func):
 	with plot_data.lock:
-		plot_data.devx = plot_data.devy = []
+		plot_data.devx, plot_data.devy = [], []
+		plot_data.needRedraw = True
 		
-		for i in range(-100, 100):
-			plot_data.devx.append(i)
-			plot_data.devy.append(func(i))
+		# for i in range(-100, 100):
+		for i_int in range(-100, 100):
+			precision = 100
+			for i_point in range(precision):
+				x = i_int + (i_point / precision)
+				# print('#', x)
+				try:
+					xval, yval = x, func(x)
+				
+				except:
+					pass
+				
+				else:
+					plot_data.devx.append(xval)
+					plot_data.devy.append(yval)
+					# print('#', xval, yval)
+					
+				# print('#', i, func(i))
+		
+		plt.clf()
+		plt.plot(plot_data.devx, plot_data.devy, label='f(x)')
+		plt.draw()
+		# plt.pause(0.01)
 
 def startPlot():
-	plt.ion()
-	plt.xlabel('x')
-	plt.ylabel('f(x)')
-	plt.title('Plot of f(x)')
+	plt.ioff()
 	
 	setFunction(lambda x: x)
 	
 	plt.show()
-	while True:
-		with plot_data.lock:
-			plt.clf()
-			plt.plot(plot_data.devx, plot_data.devy, label='f(x)')
+	# while not shell_exit:
+		# if plot_data.needRedraw:
+			# with plot_data.lock:
+				# plt.cla()
+				
+				# plt.xlabel('x')
+				# plt.ylabel('f(x)')
+				# plt.title('Plot of f(x)')
+				
+				# plt.plot(plot_data.devx, plot_data.devy, label='f(x)')
+				# plt.draw()
+				
+				# plot_data.needRedraw = False
 		
-		plt.draw()
-		plt.pause(0.01)
-		time.sleep(0.01)
+		# plt.pause(0.01)
+		# time.sleep(0.01)
+		# print('Now updating.', shell_exit)
 	
-	plt.close()
+	# plt.close()
+
+def interactiveShell():
+	global shell_exit
+	
+	shell = code.InteractiveConsole({**globals(), **locals()})
+	shell.interact()
+	
+	# print('Well, trying to close...')
+	shell_exit = True
 
 def main():
-	threading.Thread(target=startPlot).start()
+	threading.Thread(target=interactiveShell).start()
+	startPlot()
 
 if __name__=="__main__":
 	main()
