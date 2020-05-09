05407191f625fe6664b86ffa6099274d05e8e9ef
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-05-09 16:51:10 +0800

    Fixing funcs.

--- show_graph.py
+++ show_graph.py
@@ -4,74 +4,51 @@ import numpy as np
 import threading, time
 import code
 
-plot_data = type('PlotData', (object,), dict(devx=[], devy=[], needRedraw=False, lock=threading.Lock())) ( )
-shell_exit = False
-
 def setFunction(func):
-	with plot_data.lock:
-		plot_data.devx, plot_data.devy = [], []
-		plot_data.needRedraw = True
-		
-		# for i in range(-100, 100):
-		for i_int in range(-100, 100):
-			precision = 100
-			for i_point in range(precision):
-				x = i_int + (i_point / precision)
-				# print('#', x)
-				try:
-					xval, yval = x, func(x)
-				
-				except:
-					pass
-				
-				else:
-					plot_data.devx.append(xval)
-					plot_data.devy.append(yval)
-					# print('#', xval, yval)
-					
-				# print('#', i, func(i))
-		
-		plt.clf()
-		plt.plot(plot_data.devx, plot_data.devy, label='f(x)')
-		plt.draw()
-		# plt.pause(0.01)
-
-def startPlot():
-	plt.ioff()
+	""" Set the plotted function.
 	
-	setFunction(lambda x: x)
+	Parameters:
+		func - The function f to be plotted.
+	"""
+	devx, devy = [], []
 	
-	plt.show()
-	# while not shell_exit:
-		# if plot_data.needRedraw:
-			# with plot_data.lock:
-				# plt.cla()
-				
-				# plt.xlabel('x')
-				# plt.ylabel('f(x)')
-				# plt.title('Plot of f(x)')
-				
-				# plt.plot(plot_data.devx, plot_data.devy, label='f(x)')
-				# plt.draw()
-				
-				# plot_data.needRedraw = False
-		
-		# plt.pause(0.01)
-		# time.sleep(0.01)
-		# print('Now updating.', shell_exit)
+	for i_int in range(-100, 100):
+		precision = 100
+		for i_point in range(precision):
+			x = i_int + (i_point / precision)
+			try:
+				xval, yval = x, func(x)
+			
+			except:
+				pass
+			
+			else:
+				devx.append(xval)
+				devy.append(yval)
 	
-	# plt.close()
-
-def interactiveShell():
-	global shell_exit
+	plt.clf()
+	plt.xlabel('x')
+	plt.ylabel('f(x)')
+	plt.title('Plot of f(x)')
 	
-	shell = code.InteractiveConsole({**globals(), **locals()})
-	shell.interact()
-	
-	# print('Well, trying to close...')
-	shell_exit = True
+	plt.plot(devx, devy, label='f(x)')
+	plt.legend()
+	plt.draw()
 
 def main():
+	""" Start the plotting functions.
+	Raises exception if called more than once.
+	"""
+	def startPlot():
+		plt.ioff()
+		
+		setFunction(lambda x: x)
+		plt.show()
+	
+	def interactiveShell():
+		shell = code.InteractiveConsole({**globals(), **locals()})
+		shell.interact()
+	
 	threading.Thread(target=interactiveShell).start()
 	startPlot()
 
